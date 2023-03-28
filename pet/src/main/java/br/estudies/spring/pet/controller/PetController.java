4de90f3afad4cca92857ba8af78e84fc3168f3bd
MarcosGOFavaretto <marcosfavaretto.dev@gmail.com>
2023-03-28 00:18:14 -0300

    :books:DOCS: Adds javadoc on bothclasses of PET API

--- pet/src/main/java/br/estudies/spring/pet/controller/PetController.java
+++ pet/src/main/java/br/estudies/spring/pet/controller/PetController.java
@@ -6,15 +6,43 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * That class is a Rest Controller, which allow us to make HTTP requests.
+ * 
+ * Annotations:
+ * - @RestController: Declares that class as a RESTFull Controller,
+ * making it responsible for deal with HTTP requests.
+ * - @RequestMapping: Defines the end-point that class has to listen to.
+ * - @GetMapping: Defines which method has to be executed when a GET
+ * request is received.
+ * - @PostMapping: Defines which method has to be executed when a POST
+ * request is received.
+ * - @RequestBody: Populate a parameter with the request body provided.
+ * 
+ * @author MarcosGOFavaretto (marcosfavaretto.dev@gmail.com)
+ *
+ */
 @RestController
 @RequestMapping("/pets")
 public class PetController {
 
+	/**
+	 * 
+	 * That is the implementation of GET HTTP verb.
+	 * 
+	 * @return a message saying "something..." for everybody.
+	 */
 	@GetMapping
 	public String bark() {
 		return "Something...";
 	}
 
+	/**
+	 * 
+	 * That is the implementation of POST HTTP verb.
+	 * 
+	 * @param body: is the provided JSON body of recived request.
+	 */
 	@PostMapping
 	public void createPet(@RequestBody String body) {
 		System.out.println(body);
