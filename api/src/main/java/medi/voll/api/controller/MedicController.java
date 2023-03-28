8f55a685b226b0935c703da77b75de199db38f8e
MarcosGOFavaretto <marcosfavaretto.dev@gmail.com>
2023-03-28 00:14:22 -0300

    :rocket:FEATURE: Creates the Voll Med API project following the concepts presented by Alura course

---
+++ api/src/main/java/medi/voll/api/controller/MedicController.java
@@ -0,0 +1,32 @@
+package medi.voll.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * That class is a Controller which allow us to make HTTP requests.
+ * 
+ * Annotations:
+ * - @RestController: Declares that class as a RESTFull Controller,
+ * making it responsible for deal with HTTP requests.
+ * - @RequestMapping: Defines the end-point that class has to listen
+ * to.
+ * - @PostMapping: Defines which method has to be executed when a POST
+ * request is received.
+ * - @RequestBody: Populate a parameter with the request body provided.
+ * 
+ * @author MarcosGOFavaretto (marcosfavaretto.dev@gmail.com)
+ *
+ */
+@RestController
+@RequestMapping("/medics")
+public class MedicController {
+
+	@PostMapping
+	public void createMedic(@RequestBody String body) {
+		System.out.println(body);
+	}
+
+}
