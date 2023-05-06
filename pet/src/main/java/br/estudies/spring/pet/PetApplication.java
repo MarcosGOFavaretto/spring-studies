f21f67b920844e56095a076b211daf2ca4dfed74
Marcos Gabriel de Oliveira Favaretto <marcosfavaretto.dev@gmail.com>
2023-05-06 21:05:23 +0000

    :ok_hand:REFACTOR: Removes blank space from package definition of PetApplication class

--- pet/src/main/java/br/estudies/spring/pet/PetApplication.java
+++ pet/src/main/java/br/estudies/spring/pet/PetApplication.java
@@ -1,4 +1,4 @@
-package br.estudies.spring.pet;
+  package br.estudies.spring.pet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
