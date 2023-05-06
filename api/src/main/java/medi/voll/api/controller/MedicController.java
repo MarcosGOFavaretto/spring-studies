e0b84c485c06f47d87952141563daa70d775164f
Marcos Gabriel de Oliveira Favaretto <marcosfavaretto.dev@gmail.com>
2023-05-06 20:56:25 +0000

    :books:DOCS: Removes all studies annotations from Voll Med API

--- api/src/main/java/medi/voll/api/controller/MedicController.java
+++ api/src/main/java/medi/voll/api/controller/MedicController.java
@@ -7,21 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import medi.voll.api.medic.MedicData;
 
-/**
- * That class is a Controller which allow us to make HTTP requests.
- * 
- * Annotations:
- * - @RestController: Declares that class as a RESTFull Controller,
- * making it responsible for deal with HTTP requests.
- * - @RequestMapping: Defines the end-point that class has to listen
- * to.
- * - @PostMapping: Defines which method has to be executed when a POST
- * request is received.
- * - @RequestBody: Populate a parameter with the request body provided.
- * 
- * @author MarcosGOFavaretto (marcosfavaretto.dev@gmail.com)
- *
- */
 @RestController
 @RequestMapping("/medics")
 public class MedicController {
