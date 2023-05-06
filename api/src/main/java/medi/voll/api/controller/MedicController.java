140275b429abc4ef9116ea95d32d403fc0a54f8e
Marcos Gabriel de Oliveira Favaretto <marcosfavaretto.dev@gmail.com>
2023-05-06 20:58:51 +0000

    :rocket:FEATURE: Implements a Get method for Medics and Patients

--- api/src/main/java/medi/voll/api/controller/MedicController.java
+++ api/src/main/java/medi/voll/api/controller/MedicController.java
@@ -1,5 +1,6 @@
 package medi.voll.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,11 @@ import medi.voll.api.medic.MedicData;
 @RestController
 @RequestMapping("/medics")
 public class MedicController {
+
+	@GetMapping
+	public String sayWelcome() {
+		return "Welcome to Medics Voll Med API!";
+	}
 
 	@PostMapping
 	public void createMedic(@RequestBody MedicData data) {
--- api/src/main/java/medi/voll/api/controller/PatientController.java
+++ api/src/main/java/medi/voll/api/controller/PatientController.java
@@ -1,5 +1,6 @@
 package medi.voll.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,11 @@ import medi.voll.api.patient.PatientData;
 @RestController
 @RequestMapping("/patient")
 public class PatientController {
+
+    @GetMapping
+	public String sayWelcome() {
+		return "Welcome to Patients Voll Med API!";
+	}
 
     @PostMapping
     public void createPatient(@RequestBody PatientData body) {
